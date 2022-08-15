23e14c854817959f92c8762feaf942b5da90c593
StarfallV <63696126+StarfallV@users.noreply.github.com>
2022-08-16 05:18:34 +0700

    Update BitMath.spec.ts

--- test/BitMath.spec.ts
+++ test/BitMath.spec.ts
@@ -35,7 +35,12 @@ describe('BitMath', () => {
     it('2', async () => {
       expect(await bitMath.mostSignificantBit(2)).to.eq(1)
     })
-    
+    it('all powers of 2', async () => {
+      const results = await Promise.all(
+        [...Array(255)].map((_, i) => bitMath.mostSignificantBit(BigNumber.from(2).pow(i)))
+      )
+      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
+    })
     it('uint256(-1)', async () => {
       expect(await bitMath.mostSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(255)
     })
@@ -60,7 +65,12 @@ describe('BitMath', () => {
     it('2', async () => {
       expect(await bitMath.leastSignificantBit(2)).to.eq(1)
     })
-    
+    it('all powers of 2', async () => {
+      const results = await Promise.all(
+        [...Array(255)].map((_, i) => bitMath.leastSignificantBit(BigNumber.from(2).pow(i)))
+      )
+      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
+    })
     it('uint256(-1)', async () => {
       expect(await bitMath.leastSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(0)
     })
