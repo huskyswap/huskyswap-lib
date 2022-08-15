da6ad1a9b33de88501af0973c1c61c4d8d387156
StarfallV <63696126+StarfallV@users.noreply.github.com>
2022-08-16 03:35:24 +0700

    update test

--- test/BitMath.spec.ts
+++ test/BitMath.spec.ts
@@ -35,12 +35,7 @@ describe('BitMath', () => {
     it('2', async () => {
       expect(await bitMath.mostSignificantBit(2)).to.eq(1)
     })
-    it('all powers of 2', async () => {
-      const results = await Promise.all(
-        [...Array(255)].map((_, i) => bitMath.mostSignificantBit(BigNumber.from(2).pow(i)))
-      )
-      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
-    })
+    
     it('uint256(-1)', async () => {
       expect(await bitMath.mostSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(255)
     })
@@ -65,12 +60,7 @@ describe('BitMath', () => {
     it('2', async () => {
       expect(await bitMath.leastSignificantBit(2)).to.eq(1)
     })
-    it('all powers of 2', async () => {
-      const results = await Promise.all(
-        [...Array(255)].map((_, i) => bitMath.leastSignificantBit(BigNumber.from(2).pow(i)))
-      )
-      expect(results).to.deep.eq([...Array(255)].map((_, i) => i))
-    })
+    
     it('uint256(-1)', async () => {
       expect(await bitMath.leastSignificantBit(BigNumber.from(2).pow(256).sub(1))).to.eq(0)
     })
